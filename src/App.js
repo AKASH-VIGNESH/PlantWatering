aecc7a39f28f45a32100ab587fad9654ea526154
AKASH-VIGNESH <akashvignesh.o@gmail.com>
2025-08-21 22:44:02 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Alo_vera from './Assets/Aloe_vera.jpg';
+import moblieAlovera from './Assets/MobileAlovera.jpg';
+import rosePlant from './Assets/Rose.png';
+import mobileRose from './Assets/MobileRose.png'
+import Card from "./Component/Card";
+import PlantDetails from "./Component/PlantDetail";
 
 function App() {
+  const [detailVisiblity, setdetailVisibility] = useState(false);
+  const [requiredDetail, setrequiredDetail] = useState(0);
+
+  const HandleClick = (index) => {
+    setdetailVisibility(true);
+    setrequiredDetail(index);
+  };
+
+  const closeDeatails = () => {
+    setdetailVisibility(false);
+  };
+
+  const data = [
+    {
+      "Name": "Alovera",
+      "logo": Alo_vera,
+      "mobileLogo": moblieAlovera,
+      "waterDuration": "every 7-10 Days",
+      "lastWatered": "Did not water",
+      "Who Watered": "No one",
+      "Tip": "Always let soil dry completely before watering again."
+    },
+    {
+      "Name": "Rose Plant",
+      "logo": rosePlant,
+      "mobileLogo": mobileRose,
+      "waterDuration": "every 7-10 Days",
+      "lastWatered": "Did not water",
+      "Who Watered": "No one",
+      "Tip": "Always let soil dry completely before watering again."
+    }
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {detailVisiblity ? (
+        <div id="PlantDetails">
+          <PlantDetails 
+            onClose={closeDeatails} 
+            plantData={data[requiredDetail]} 
+          /> 
+        </div>
+      ) : (
+        <div id="plantList">
+          {data.map((e, index) => (
+            <Card
+              key={index}
+              lastWatered={e.lastWatered}
+              name={e.Name}
+              logo={e.logo}
+              mobileLogo={e.mobileLogo}
+              onClick={() => HandleClick(index)}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
