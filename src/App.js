5f970fa57a89a4e63f3cf991d0d23128581122fd
AKASH-VIGNESH <akashvignesh.o@gmail.com>
2025-08-31 16:09:09 +0530

    Design Reset

--- src/App.js
+++ src/App.js
@@ -7,61 +7,9 @@ import Card from "./Component/Card";
 import PlantDetails from "./Component/PlantDetail";
 import './App.css';
 function App() {
-  const [detailVisiblity, setdetailVisibility] = useState(false);
-  const [requiredDetail, setrequiredDetail] = useState(0);
-
-  const HandleClick = (index) => {
-    setdetailVisibility(true);
-    setrequiredDetail(index);
-  };
-
-  const closeDeatails = () => {
-    setdetailVisibility(false);
-  };
-
-  const data = [
-    {
-      "Name": "Alovera",
-      "logo": Alo_vera,
-      "mobileLogo": moblieAlovera,
-      "waterDuration": "every 7-10 Days",
-      "lastWatered": "Did not water",
-      "Who Watered": "No one",
-      "Tip": "Always let soil dry completely before watering again."
-    },
-    {
-      "Name": "Rose Plant",
-      "logo": rosePlant,
-      "mobileLogo": mobileRose,
-      "waterDuration": "every 7-10 Days",
-      "lastWatered": "Did not water",
-      "Who Watered": "No one",
-      "Tip": "Always let soil dry completely before watering again."
-    }
-  ];
   return (
     <div>
-      {detailVisiblity ? (
-        <div id="PlantDetails">
-          <PlantDetails 
-            onClose={closeDeatails} 
-            plantData={data[requiredDetail]} 
-          /> 
-        </div>
-      ) : (
-        <div id="plantList">
-          {data.map((e, index) => (
-            <Card
-              key={index}
-              lastWatered={e.lastWatered}
-              name={e.Name}
-              logo={e.logo}
-              mobileLogo={e.mobileLogo}
-              onClick={() => HandleClick(index)}
-            />
-          ))}
-        </div>
-      )}
+      
     </div>
   );
 }
